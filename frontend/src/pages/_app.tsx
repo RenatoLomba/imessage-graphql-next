81358a910920026bc1346e5a0120f4f6cb5bbdd3
RenatoLomba <rntlomba@gmail.com>
2022-12-28 22:52:51 -0300

    setup apollo client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
+import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { apolloClient } from '../lib/apollo'
 import { theme } from '../lib/chakra'
 
 export default function App({
@@ -10,10 +12,12 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={apolloClient}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   )
 }
