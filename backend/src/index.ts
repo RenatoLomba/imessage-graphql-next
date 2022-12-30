1e2ca9d8c6bfdb4cabe30ec2693cec335362688e
RenatoLomba <rntlomba@gmail.com>
2022-12-29 23:31:59 -0300

    cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import {
   ApolloServerPluginLandingPageLocalDefault,
 } from 'apollo-server-core'
 import { ApolloServer } from 'apollo-server-express'
+import * as dotenv from 'dotenv'
 import express from 'express'
 import http from 'http'
 
@@ -12,6 +13,7 @@ import { resolvers } from './graphql/resolvers'
 import { typeDefs } from './graphql/types'
 
 async function bootstrap() {
+  dotenv.config()
   const app = express()
   const httpServer = http.createServer(app)
 
@@ -30,7 +32,13 @@ async function bootstrap() {
     ],
   })
   await server.start()
-  server.applyMiddleware({ app })
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: process.env.CLIENT_ORIGIN,
+      credentials: true,
+    },
+  })
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: 5000 }, resolve),
   )
