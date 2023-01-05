696856b6e10effaefb1bc234593130975db2a633
RenatoLomba <rntlomba@gmail.com>
2023-01-05 18:33:15 -0300

    setup ws subscriptions on client

--- apps/frontend/src/lib/apollo.ts
+++ apps/frontend/src/lib/apollo.ts
@@ -1,11 +1,40 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { Kind, OperationTypeNode } from 'graphql'
+import { createClient } from 'graphql-ws'
+
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities'
 
 const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URI,
   credentials: 'include',
 })
 
+const wsLink =
+  typeof window !== 'undefined'
+    ? new GraphQLWsLink(
+        createClient({
+          url: process.env.NEXT_PUBLIC_GRAPHQL_WS_URL!,
+        }),
+      )
+    : null
+
+const link =
+  typeof window !== 'undefined' && !!wsLink
+    ? split(
+        ({ query }) => {
+          const definition = getMainDefinition(query)
+          return (
+            definition.kind === Kind.OPERATION_DEFINITION &&
+            definition.operation === OperationTypeNode.SUBSCRIPTION
+          )
+        },
+        wsLink,
+        httpLink,
+      )
+    : httpLink
+
 export const apolloClient = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache(),
 })
